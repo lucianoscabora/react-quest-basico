7aa40d217996397aca584be3bbdef29e30a44cc2
lucianoscabora <lucianoscabora@gmail.com>
2023-02-11 21:14:18 -0300

    desafio: desafio numero 1 concluido

---
+++ src/components/paragrafoMaiusculoColorido.js
@@ -0,0 +1,18 @@
+import '../components/paragrafoMaiusculoColorido.css'
+
+const ParagrafoMaiusculoColorido = ({paragraphColor, upperCaseParagraph}) => {
+    return (
+    <div>
+    <p className="p-color-uppercase" style={{color: paragraphColor, textTransform: upperCaseParagraph}}>
+        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+    </p>
+    </div>
+    )
+}
+
+ParagrafoMaiusculoColorido.defaultProps = {
+    paragraphColor: "blue",
+    upperCaseParagraph: "uppercase"
+};
+
+export default ParagrafoMaiusculoColorido
